ffea58cc52dee06f40cd2abeb3d4f23f466db8e4
zws910 <vincentzheng0910@gmail.com>
2018-09-18 19:31:20 +0800

    fix - bugs in areas urls

--- meiduo_mall/meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/meiduo_mall/apps/oauth/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     url('^qq/authorization/$', views.QQAuthURLView.as_view()),
     url('^qq/user/$', views.QQAuthUserView.as_view()),
-}
+]
