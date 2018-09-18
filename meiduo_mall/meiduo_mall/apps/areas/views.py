45637795bb27cacc0047bd18394e9576eb7d45b4
zws910 <vincentzheng0910@gmail.com>
2018-09-18 18:54:01 +0800

    add - areas app

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from .models import Area
+from . import serializers
+
+
+# GET /areas/     GET /areas/<pk>/
+class AreasViewSet(ReadOnlyModelViewSet):
+    """
+    list: 返回所有省份的信息
+    retrieve: 返回特定省或市的下属行政规划区域
+    """
+
+    # queryset = Area.objects.all()
+    def get_queryset(self):
+        if self.action == "list":
+            return Area.objects.filter(parent=None)
+        else:  # retrieve
+            return Area.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return serializers.AreaSerializer
+
+
