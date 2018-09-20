26b34dd0e06c7acdd44d6f69475788eaf4369476
zws910 <vincentzheng0910@gmail.com>
2018-09-20 10:12:23 +0800

    add - goods

--- meiduo_mall/meiduo_mall/apps/goods/models.py
+++ meiduo_mall/meiduo_mall/apps/goods/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 from meiduo_mall.utils.models import BaseModel
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
 
 
 class GoodsCategory(BaseModel):
@@ -69,6 +72,10 @@ class Goods(BaseModel):
                                   verbose_name='三级类别')
     sales = models.IntegerField(default=0, verbose_name='销量')
     comments = models.IntegerField(default=0, verbose_name='评价数')
+
+    desc_detail = RichTextUploadingField(default='', verbose_name='详细介绍')
+    desc_pack = RichTextField(default='', verbose_name='包装信息')
+    desc_service = RichTextUploadingField(default='', verbose_name='售后服务')
 
     class Meta:
         db_table = 'tb_goods'
